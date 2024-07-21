cfd6377820a28a9d77a0906020b99b602f16ac08
Mihail Gaberov <mihail.gaberov@gmail.com>
2024-07-21 14:15:08 +0200

    :student: Timing in the Animation System

--- src/World/systems/Loop.js
+++ src/World/systems/Loop.js
@@ -1,5 +1,7 @@
 import { Clock } from "three";
 
+const clock = new Clock();
+
 class Loop {
   constructor(camera, scene, renderer) {
     this.camera = camera;
@@ -20,8 +22,9 @@ class Loop {
   }
 
   tick() {
+    const delta = clock.getDelta();
     for (const object of this.updatables) {
-      object.tick();
+      object.tick(delta);
     }
   }
 }
