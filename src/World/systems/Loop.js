d487f430fb25455ee87ffd7bc8d79ee9fa83aa5c
Mihail Gaberov <mihail.gaberov@gmail.com>
2024-07-21 13:58:13 +0200

    :student: rotate the cube

--- src/World/systems/Loop.js
+++ src/World/systems/Loop.js
@@ -5,16 +5,24 @@ class Loop {
     this.camera = camera;
     this.scene = scene;
     this.renderer = renderer;
+    this.updatables = [];
   }
 
   start() {
     this.renderer.setAnimationLoop(() => {
+      this.tick();
       this.renderer.render(this.scene, this.camera);
     });
   }
 
   stop() {
     this.renderer.setAnimationLoop(null);
+  }
+
+  tick() {
+    for (const object of this.updatables) {
+      object.tick();
+    }
   }
 }
 
