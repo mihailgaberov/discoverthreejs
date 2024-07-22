f55dfb9871e66af66496a73402827b58b5efd900
Mihail Gaberov <mihail.gaberov@gmail.com>
2024-07-22 11:41:01 +0300

    :student: remove comments

--- src/World/components/lights.js
+++ src/World/components/lights.js
@@ -1,7 +1,6 @@
 import { DirectionalLight } from "three";
 
 function createLights() {
-  // Create a directional light
   const light = new DirectionalLight("white", 8);
 
   // move the light right, up, and towards us
