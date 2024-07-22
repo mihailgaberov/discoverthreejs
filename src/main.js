ce485e524ffbad4ef738c5e443f5e9597845e326
Mihail Gaberov <mihail.gaberov@gmail.com>
2024-07-22 12:51:43 +0300

    :student: add toggle animation button

--- src/main.js
+++ src/main.js
@@ -2,10 +2,22 @@ import { World } from "./World/World.js";
 
 function main() {
   const container = document.querySelector("#scene-container");
-
+  const toggleButton = document.querySelector("#toggle-button");
   const world = new World(container);
+  let isRunning = false;
 
-  world.start();
+  toggleButton.addEventListener("click", () => {
+    console.log("toggle", world);
+    if (isRunning) {
+      world.stop();
+      isRunning = false;
+      toggleButton.textContent = "Start";
+    } else {
+      world.start();
+      isRunning = true;
+      toggleButton.textContent = "Stop";
+    }
+  });
 }
 
 main();
