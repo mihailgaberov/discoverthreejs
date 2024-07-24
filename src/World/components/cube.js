cbb7bd99bde900da3f9855642359a9da82dc4bd6
Mihail Gaberov <mihail.gaberov@gmail.com>
2024-07-24 12:50:21 +0300

    :student: introduce textures

--- src/World/components/cube.js
+++ src/World/components/cube.js
@@ -1,14 +1,24 @@
-import { BoxGeometry, Mesh, MeshStandardMaterial } from "three";
+import { BoxGeometry, Mesh, MeshStandardMaterial, TextureLoader } from "three";
 import { MathUtils } from "three";
+
+function createMaterial() {
+  // create a texture loader.
+  const textureLoader = new TextureLoader();
+
+  // load a texture
+  const texture = textureLoader.load("/assets/textures/uv-test-bw.png");
+
+  // create a "standard" material
+  const material = new MeshStandardMaterial({ map: texture });
+
+  return material;
+}
 
 function createCube(container) {
   // create a geometry
   const geometry = new BoxGeometry(2, 2, 2);
 
-  // create a default (white) Basic material
-  const material = new MeshStandardMaterial({
-    color: "purple",
-  });
+  const material = createMaterial();
 
   // create a Mesh containing the geometry and material
   const cube = new Mesh(geometry, material);
