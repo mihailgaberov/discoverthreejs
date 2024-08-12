de68a270d5fa183e2446e5ac4e3588cb600a34dc
Mihail Gaberov <mihail.gaberov@gmail.com>
2024-08-12 09:59:41 +0200

    :student: create clones, update positions and rotation

--- src/World/components/meshGroup.js
+++ src/World/components/meshGroup.js
@@ -15,9 +15,29 @@ function createMeshGroup() {
   });
   const protoSphere = new Mesh(geometry, material);
 
+  // create twenty clones of the protoSphere
+  // and add each to the group
+  for (let i = 0; i < 1; i += 0.05) {
+    const sphere = protoSphere.clone();
+    // position the spheres on around a circle
+    sphere.position.x = Math.cos(2 - Math.PI - i);
+    sphere.position.y = Math.sin(2 - Math.PI - i);
+    sphere.scale.multiplyScalar(0.01 + i);
+
+    group.add(sphere);
+  }
   // add the sphere to the group
   group.add(protoSphere);
-  group.tick = (delta) => {};
+  // every sphere inside the group will be scaled
+  group.scale.multiplyScalar(2);
+
+  // spin the wheel
+  const radiansPerSecond = MathUtils.degToRad(30);
+
+  // each frame, rotate the entire group of spheres
+  group.tick = (delta) => {
+    group.rotation.z -= delta * radiansPerSecond;
+  };
 
   return group;
 }
